0db639865e4bd8b6867f7d94d67f9b4210af76ac
Danny <51312422+dchenz@users.noreply.github.com>
2025-07-12 10:58:25 +0000

    Rename to jupyter-notebook

--- jupyter-notebook/build-push.sh
+++ jupyter-notebook/build-push.sh
@@ -5,7 +5,8 @@ set -e
 PYTHON_BASE=3.12-alpine
 JUPYTER_VERSION=7.4.2
 COMMIT_HASH=$(git rev-parse --short HEAD)
-IMAGE_NAME=dchenz/jupyter-notebook-kernelless:$PYTHON_BASE-$JUPYTER_VERSION-$COMMIT_HASH
+
+IMAGE_NAME=dchenz/jupyter-notebook:$PYTHON_BASE-$JUPYTER_VERSION-$COMMIT_HASH
 
 docker build . -t $IMAGE_NAME \
     --build-arg PYTHON_BASE=$PYTHON_BASE \
